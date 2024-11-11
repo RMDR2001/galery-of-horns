d6ce1115bc030c9d17f07ed4c9ce4ae8357238c3
RicardoMauricioDelgadoRivera <rmdr2001@gmail.com>
2024-11-11 16:14:21 -0500

    2da funcionalidad

--- src/component/HornedBeast.jsx
+++ src/component/HornedBeast.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 function HornedBeast(props) {
+
+    const [likes, setLikes] = useState(0);
+    
+    const añadirLike = () => {
+        setLikes(likes + 1);
+    }
     return (
         <>
             <h2>{props.title}</h2>
-            <img src={props.imageURL} alt={props.description} title={props.title} />
-            <p>{props.description}</p>
+            <img src={props.imageURL} alt={props.description} title={props.title} height="300px"/>
+            <p>{props.descriptions}</p>
+            <button onClick={añadirLike}>{likes} ❤️</button>
         </>
     )
 }
