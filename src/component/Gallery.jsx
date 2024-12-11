a97deaa492cb485635c2d946ef2e808b881dff42
RicardoMauricioDelgadoRivera <rmdr2001@gmail.com>
2024-12-10 19:40:44 -0500

    Logro adicional

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,15 +1,34 @@
 // Gallery.jsx
+import { useState } from "react";
 import HornedBeast from './HornedBeast';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Gallery({ beasts, actualizarBeasts, onSelectBeast }) {
+  const [searchTerm, setSearchTerm] = useState("");  // Estado para el término de búsqueda
+
+  // Filtrar las bestias en función del término de búsqueda usando expresión regular
+  const filteredBeasts = beasts.filter((beast) => {
+    const regex = new RegExp(searchTerm, 'i');  // Crea la expresión regular con el término de búsqueda
+    return (
+      regex.test(beast.title) || regex.test(beast.keyword) // Verifica si el término de búsqueda está en el título o palabra clave
+    );
+  });
 
   return (
     <Container>
+      {/* Campo de búsqueda */}
+      <input
+        type="text"
+        placeholder="Buscar por título o palabra clave"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)} // Actualizar el estado con lo que escribe el usuario
+        className="form-control mb-4"
+      />
+
       <Row xs={2} sm={3} lg={5}>
-        {beasts.map(beast => (
+        {filteredBeasts.map(beast => (
           <Col key={beast._id}>
             <HornedBeast
               title={beast.title}
